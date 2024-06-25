b1c322896d8b2f5c3ba661c1edb2c2a88d28565d
Victor Bascope <juan.victor.bascope.castro@gmail.com>
2024-06-25 11:05:44 -0400

    configuracion para git-hub-pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/ita',
+    base: 'https://juanvictorbascopecastro.github.io/block-app-practicas',
     define: {
         'process.env': process.env,
     },
